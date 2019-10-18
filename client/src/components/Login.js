adcf8f9f526fec46af271f8d538b7e0a1cb9509b
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-18 11:18:31 -0400

    Creates login form with handleChange

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+    console.log(credentials)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    //axios with auth
+    
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <form onSubmit={handleSubmit}>
+        <input type='text' name='username' onChange={handleChange}></input>
+        <input type='password' name='password' onChange={handleChange}></input>
+        <button>Log in</button>
+    </form>
   );
 };
 
